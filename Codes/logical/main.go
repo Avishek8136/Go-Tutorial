d28ba96d6120c21ea29db4ab3689a5a3c2cab2d5
Avishek8136 <avishekrauniyar07@gmail.com>
2025-11-22 23:23:24 +0530

    Logical operators in Go

---
+++ Codes/logical/main.go
@@ -0,0 +1,24 @@
+package main
+
+func main() {
+	//bitwise operators
+	println(5 | 3)     //bitwise OR
+	println(5 & 3)     //bitwise AND
+	println(5 ^ 3)     //bitwise XOR
+	println(5 &^ 3)    //bit clear
+	println(5000 >> 3) //right shift
+	println(5000 << 3) //left shift
+}
+
+// func main() {
+// 	number := 5
+// 	if number < 5 && number > 0 {
+// 		fmt.Println("The number is between 1 and 4")
+// 	} else if number == 5 || number == 0 {
+// 		fmt.Println("The number is 5 or 0")
+// 	} else if !(number == 8) {
+// 		fmt.Println("The number is not 8")
+// 	} else {
+// 		fmt.Println("The number is greater than 5 or less than 1")
+// 	}
+// }
